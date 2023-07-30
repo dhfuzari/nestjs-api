f7eac11e168247980ec1b17fa0c948ed77981ef7
Daniel Fuzari <daniel.fuzari@ifood.com.br>
2023-07-29 22:13:32 -0300

    feat: Create CRUD operations for the User resource

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  @Post()
+  async create(@Body() body) {
+    return await { ...body };
+  }
+
+  @Get()
+  async read() {
+    return await { users: [] };
+  }
+
+  @Get(':id')
+  async readOne(@Param() param) {
+    return await { user: { ...param } };
+  }
+
+  @Put(':id')
+  async update(@Param() param, @Body() body) {
+    return {
+      method: 'PUT',
+      body,
+      param,
+    };
+  }
+
+  @Patch(':id')
+  async updatePartial(@Param() param, @Body() body) {
+    return {
+      method: 'PATCH',
+      body,
+      param,
+    };
+  }
+
+  @Delete(':id')
+  async delete(@Param() param) {
+    return {
+      ...param,
+    };
+  }
+}
