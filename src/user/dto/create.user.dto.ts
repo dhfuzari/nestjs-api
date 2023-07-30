ae5d24ab00c81783c02bcebbcbef940f38f653d1
Daniel Fuzari <daniel.fuzari@ifood.com.br>
2023-07-30 14:58:49 -0300

    feature: Apply validation with CreateUserDTO

---
+++ src/user/dto/create.user.dto.ts
@@ -0,0 +1,18 @@
+import { IsEmail, IsString, IsStrongPassword } from 'class-validator';
+
+export class CreateUserDTO {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsStrongPassword({
+    minLength: 6,
+    minLowercase: 1,
+    minNumbers: 1,
+    minUppercase: 1,
+    minSymbols: 0,
+  })
+  password: string;
+}
