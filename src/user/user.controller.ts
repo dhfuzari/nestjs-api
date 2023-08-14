ddd6a1026f22713c2add27e0c6713d0d3e14be79
Daniel Fuzari <daniel.fuzari@ifood.com.br>
2023-08-14 11:58:09 -0300

    feat: Create custom param decorator

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,8 +3,6 @@ import {
   Controller,
   Delete,
   Get,
-  Param,
-  ParseIntPipe,
   Patch,
   Post,
   Put,
@@ -13,6 +11,7 @@ import { CreateUserDTO } from './dto/create.user.dto';
 import { UpdateTotalUserDTO } from './dto/update-total.user.dto';
 import { UpdatePartialUserDTO } from './dto/update-partial.user.dto';
 import { UserService } from './user.service';
+import { ParamIdDecorator } from 'src/decorators/param-id.decorator';
 
 @Controller('users')
 export class UserController {
@@ -29,13 +28,13 @@ export class UserController {
   }
 
   @Get(':id')
-  async readOne(@Param('id', ParseIntPipe) id: number) {
+  async readOne(@ParamIdDecorator() id: number) {
     return await this.userService.readById(id);
   }
 
   @Put(':id')
   async updateTotal(
-    @Param('id', ParseIntPipe) id: number,
+    @ParamIdDecorator() id: number,
     @Body() body: UpdateTotalUserDTO,
   ) {
     return await this.userService.updateTotal(id, body);
@@ -43,14 +42,14 @@ export class UserController {
 
   @Patch(':id')
   async updatePartial(
-    @Param('id', ParseIntPipe) id: number,
+    @ParamIdDecorator() id: number,
     @Body() body: UpdatePartialUserDTO,
   ) {
     return await this.userService.updatePartial(id, body);
   }
 
   @Delete(':id')
-  async delete(@Param('id', ParseIntPipe) id: number) {
+  async delete(@ParamIdDecorator() id: number) {
     return await this.userService.delete(id);
   }
 }
